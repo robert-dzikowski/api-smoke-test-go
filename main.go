fe8deb13b3cbed13cfd14696028ceaf5e39dcf0a
Robert Dzikowski <rdzikowski@live.com>
2023-07-04 23:35:27 +0200

    Added all cmd line arguments

--- main.go
+++ main.go
@@ -8,8 +8,16 @@ import (
 
 func main() {
 	oasFile := flag.String(
-		"oas", "", "Url or file name of the OpenAPI v.3 specification file")
+		"oas", "", "Required, url or file name of the OpenAPI v.3 specification file")
+	auth := flag.Bool("auth", false,
+		"Use authentication, i.e. authentication token is used")
+	localhost := flag.Bool("localhost", false,
+		"Use when testing API that runs on your machine")
+	onlyGet := flag.Bool("only-get", false, "Test only GET requests")
+	requestParam := flag.Int("req-param", 13,
+		"Value used in requests that contain parameters")
 	help := flag.Bool("help", false, "Show help")
+
 	flag.Parse()
 
 	if *help {
@@ -18,11 +26,21 @@ func main() {
 	}
 
 	if *oasFile == "" {
-		fmt.Println("oas argument is required")
+		fmt.Println("\"oas\" argument is required")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
+	fmt.Println("oas:", *oasFile)
 
-	flag.Parse()
-	fmt.Println(*oasFile)
+	if *auth {
+		fmt.Println("auth:", *auth)
+	}
+	if *localhost {
+		fmt.Println("localhost:", *localhost)
+	}
+	if *onlyGet {
+		fmt.Println("only-get:", *onlyGet)
+	}
+	fmt.Println("req-param:", *requestParam)
+
 }
